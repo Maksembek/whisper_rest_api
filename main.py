164d838d19b661e1a6d8645a9b97ea5660c4e475
Maksembek <56928893+Maksembek@users.noreply.github.com>
2023-04-14 14:36:13 +0300

    Added processing of http error codes

--- main.py
+++ main.py
@@ -5,10 +5,16 @@ from whisper_ops import transcribe_to_text
 
 app = FastAPI()
 
+@app.get("/")
+def root():
+    return {"message": "Hello!"}
 
 @app.get("/translate/")
 async def create_file(url: str = None):
     doc = rq.get(url)
+    if (doc.status_code != 200):
+        return {"url_error": f"Problems with url processing! Code: {doc.status_code}" }
+        
     with open('voice.mp3', 'wb') as f:
         f.write(doc.content)
         transcribe_result = transcribe_to_text(f)
