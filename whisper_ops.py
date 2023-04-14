af869383fb0f37c55caa1007841755c9a92a1115
Maksembek <56928893+Maksembek@users.noreply.github.com>
2023-04-14 14:37:54 +0300

    Added try/except block and new processing of empty files

--- whisper_ops.py
+++ whisper_ops.py
@@ -4,13 +4,18 @@ base_model = whisper.load_model("base")
 
 
 def transcribe_to_text(file):
-    result = base_model.transcribe(file.name, fp16=False, language='ru')
-    transcribed_text = result.get("text")
+    try:
+        result = base_model.transcribe(file.name, fp16=False, language='ru')
+        transcribed_text = result.get("text")
+    except Exception as e:
+        return("Found error in work with wisper")
 
     if transcribed_text is None:
         return "Audio could not be transcribed"
 
-    if len(transcribed_text) == 0:
+    # ответ whisper по умолчанию, если был задан пустой файл
+    empty_var = " Редактор субтитров А.Семкин Корректор А.Егорова"
+    if ( (len(transcribed_text) == 0) or (transcribed_text == empty_var) ):
         return "Audio is emtpy"
 
     return transcribed_text
